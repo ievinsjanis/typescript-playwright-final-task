bc3050022d31a5417bc9fdf0919a8433872e7164
Jānis Ieviņš <janis.ievins@testdevlab.com>
2024-04-26 11:46:07 +0300

    last test case finished

--- page-objects/CartPage.ts
+++ page-objects/CartPage.ts
@@ -6,6 +6,13 @@ export default class CartPage extends BasePage {
   readonly cartItem: Locator;
   readonly checkOutButton: Locator;
   readonly payAndConfirmButton: Locator;
+  readonly cardNameInput: Locator;
+  readonly cardNumberInput: Locator;
+  readonly cardCvcCodeInput: Locator;
+  readonly cardMonthInput: Locator;
+  readonly cardYearInput: Locator;
+  readonly orderPlacementForm: Locator;
+  readonly emptyCartField: Locator;
 
   constructor(page: Page) {
     super(page);
@@ -13,6 +20,13 @@ export default class CartPage extends BasePage {
     this.checkOutButton = this.page.locator(".btn.btn-default.check_out");
     this.payAndConfirmButton = this.page.locator('[data-qa="pay-button"]');
     this.cartItem = this.page.locator("#product-1");
+    this.cardNumberInput = this.page.locator('input[name="card_number"]')
+    this.cardNameInput = this.page.locator('input[name="name_on_card"]')
+    this.cardYearInput = this.page.getByPlaceholder("YYYY")
+    this.cardMonthInput = this.page.getByPlaceholder("MM")
+    this.cardCvcCodeInput = this.page.getByPlaceholder("ex.")
+    this.orderPlacementForm = this.page.locator("#form")
+    this.emptyCartField = this.page.locator('[id="empty_cart"]')
   }
 
   async assertItemIsVisibleInCart(itemName: string, itemCategory: string, itemPrice: string) {
@@ -30,18 +44,20 @@ export default class CartPage extends BasePage {
   }
   
   async fillCreditCardInfo(name: string, card_number: string, cvc_code: string, month: string, year: string) {
-    await this.nameInput.fill(name);
+    await this.cardNameInput.fill(name);
     await this.cardNumberInput.fill(card_number);
-    await this.cvcCodeInput.fill(cvc_code);
-    await this.monthInput.fill(month);
-    await this.yearInput.fill(year);
+    await this.cardCvcCodeInput.fill(cvc_code);
+    await this.cardMonthInput.fill(month);
+    await this.cardYearInput.fill(year);
   }
 
-  async assertOrderComplete() {
-    await this.clickButton(this.payAndConfirmButton)
+  async assertOrderCompleteMessage(orderMessage: string) {
+    await this.assertText(this.orderPlacementForm, orderMessage);
   }
 
   async assertCartEmpty() {
-    await this.clickButton(this.payAndConfirmButton)
+    await this.navigateToPage("/view_cart")
+    await this.assertPageUrl("/view_cart")
+    await this.assertText(this.emptyCartField, 'Cart is empty! Click here to buy products.');
   }
 }
